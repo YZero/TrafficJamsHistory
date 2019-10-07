6eaf97c4dfa9896ddf7dfab226e0f4fbc1d5fc2e
Stepan Lushchiy <steeg.xs@gmail.com>
2019-10-08 02:15:03 +0300

    serving media via djangp

--- src/urls.py
+++ src/urls.py
@@ -5,4 +5,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
