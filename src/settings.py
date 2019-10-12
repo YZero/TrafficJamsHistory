339d4630943611878b46391e0dfc1378548fb192
Stepan Lushchiy <steeg.xs@gmail.com>
2019-10-12 21:03:22 +0300

    static root

--- src/settings.py
+++ src/settings.py
@@ -117,6 +117,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = '6379'
