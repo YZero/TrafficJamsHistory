7b44d91e360ab050b212dff444256b14cedfec76
Stepan Lushchiy <steeg.xs@gmail.com>
2019-10-12 20:31:20 +0300

    allowed hosts

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'zlls1%n&#l&q)tq%g=ax5cb6!84^w6lpo)_-_*rv5pz%(g#els'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
