203357b286f58ea71c51e9964209c54a22acc244
Stepan Lushchiy <steeg.xs@gmail.com>
2019-12-18 12:22:34 +0300

    humanize

--- src/settings.py
+++ src/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'map_shots',
     'personal_items',
     'celery',
